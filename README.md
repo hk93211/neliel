fb72bcd1909b5469241b076f0f582597e27623b4
黄鹍 <kun1.huang@meicloud.com>
2019-08-14 11:18:34 +0800

    feat: math and collection methods 1.math: add/sub/mul/div 2.collection: convertArrayToTree

---
+++ README.md
@@ -0,0 +1,14 @@
+## neliel
+
+### Installation
+
+Using npm:
+
+```javascript
+npm install -g npm
+npm install --save neliel
+```
+
+### Support
+
+Tested in Chrome 74-75, Firefox 66-67, IE 11, Edge 18, Safari 11-12, & Node.js 8-12.
---
+++ index.js
@@ -0,0 +1,162 @@
+var neliel = {
+
+  /****************************************  math  ****************************************/
+
+  /**
+   * accurate adds two numbers.
+   *
+   * @category Math
+   * @param {number} augend The first number in an addition.
+   * @param {number} addend The second number in an addition.
+   * @param {number} toFixedNumber result to fixed number.
+   * @returns {number} Returns the total.
+   * @example
+   *
+   * neliel.add(0.1, 0.7);
+   * // => 0.8
+   * neliel.add(0.22, 0.91, 2);
+   * // => 1.13
+   *
+   */
+  add: function(augend, addend) {
+    augend = (augend || 0).toString()
+    addend = (addend || 0).toString()
+    var arg1Arr = augend.split('.'),
+      arg2Arr = addend.split('.'),
+      d1 = arg1Arr.length == 2 ? arg1Arr[1] : '',
+      d2 = arg2Arr.length == 2 ? arg2Arr[1] : ''
+    var maxLen = Math.max(d1.length, d2.length)
+    var m = Math.pow(10, maxLen)
+    var result = Number(((augend * m + addend * m) / m).toFixed(maxLen))
+    var d = arguments[2]
+    return typeof d === 'number' ? Number(result.toFixed(d)) : result
+  },
+  /**
+   * accurate subtract two numbers.
+   *
+   * @category Math
+   * @param {number} minuend The first number in a subtraction.
+   * @param {number} subtrahend The second number in a subtraction.
+   * @param {number} toFixedNumber result to fixed number.
+   * @returns {number} Returns the difference.
+   * @example
+   *
+   * neliel.sub(0.8 - 0.1);
+   * // => 0.7
+   */
+  sub: function(minuend, subtrahend) {
+    return neliel.add(minuend, -Number(subtrahend), arguments[2])
+  },
+  /**
+   * accurate multiply two numbers.
+   *
+   * @category Math
+   * @param {number} multiplier The first number in a multiplication.
+   * @param {number} multiplicand The second number in a multiplication.
+   * @param {number} toFixedNumber result to fixed number.
+   * @returns {number} Returns the product.
+   * @example
+   *
+   * neliel.mul(0.8, 0.1);
+   * // => 0.08
+   */
+  mul: function(multiplier, multiplicand) {
+    var r1 = (multiplier || 0).toString(),
+      r2 = (multiplicand || 0).toString(),
+      m,
+      resultVal,
+      d = arguments[2]
+    m =
+      (r1.split('.')[1] ? r1.split('.')[1].length : 0) +
+      (r2.split('.')[1] ? r2.split('.')[1].length : 0)
+    resultVal = (Number(r1.replace('.', '')) * Number(r2.replace('.', ''))) / Math.pow(10, m)
+    return typeof d !== 'number' ? Number(resultVal) : Number(resultVal.toFixed(parseInt(d)))
+  },
+  /**
+   * accurate divide two numbers.
+   *
+   * @category Math
+   * @param {number} dividend The first number in a division.
+   * @param {number} divisor The second number in a division.
+   * @param {number} toFixedNumber result to fixed number.
+   * @returns {number} Returns the quotient.
+   * @example
+   *
+   * neliel.div(0.08, 0.1);
+   * // => 0.8
+   */
+  div: function(dividend, divisor) {
+    var r1 = (dividend || 0).toString(),
+      r2 = (divisor || 1).toString(),
+      m,
+      resultVal,
+      d = arguments[2]
+    m =
+      (r2.split('.')[1] ? r2.split('.')[1].length : 0) -
+      (r1.split('.')[1] ? r1.split('.')[1].length : 0)
+    resultVal = (Number(r1.replace('.', '')) / Number(r2.replace('.', ''))) * Math.pow(10, m)
+    return typeof d !== 'number' ? Number(resultVal) : Number(resultVal.toFixed(parseInt(d)))
+  },
+
+  /****************************************  colloction  ****************************************/
+
+  /**
+   * 
+   * @param {Array} arrayDatas The array to be converted
+   * @param {String} id The unique index of the current object
+   * @param {String} pId The unique index of the current object 's parent
+   * 
+   * @example
+   * 
+   * neliel.convertArrayToTree([
+   *    { name: 'a', id: '1', parentId: '' },
+   *    { name: 'b', id: '2', parentId: '1' },
+   *    { name: 'c', id: '3', parentId: '1' },
+   *    { name: 'd', id: '4', parentId: '2' },
+   * ], 'id', 'parentId')
+   */
+  convertArrayToTree: function(arrayDatas, id, pId) {
+    if (!id) id = 'id';
+    if (!pId) pId = 'parentId';
+    function exists(data, parentId) {
+      for (var i = 0; i < data.length; i++) {
+        var element = data[i];
+        if (element[id] == parentId) return true;
+      }
+      return false;
+    }
+  
+    var nodes = [];
+    // get the top level nodes
+    for (var i = 0; i < arrayDatas.length; i++) {
+      var row = arrayDatas[i];
+      if (!exists(arrayDatas, row[pId])) {
+        nodes.push(row);
+      }
+    }
+  
+    var toDo = [];
+    for (var i = 0; i < nodes.length; i++) {
+      toDo.push(nodes[i]);
+    }
+    while (toDo.length) {
+      var node = toDo.shift(); // the parent node
+      // get the children nodes
+      for (var i = 0; i < arrayDatas.length; i++) {
+        var row = arrayDatas[i];
+        if (row[pId] == node[id]) {
+          var child = row;
+          if (node.children) {
+            node.children.push(child);
+          } else {
+            node.children = [child];
+          }
+          toDo.push(child);
+        }
+      }
+    }
+    return nodes;
+  },
+}
+
+module.exports = neliel
